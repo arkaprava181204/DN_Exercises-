d796a99693633fd506c1f3f5fb9a482f77834d07
Arkaprava Bhattacharya <arkaprava181204@gmail.com>
2026-07-28 16:10:36 +0530

    Add files via upload

---
+++ Microservices/WebApplication1/WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using JwtAuthenticationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(User user)
+        {
+            // Dummy authentication
+            if (user.Username == "admin" && user.Password == "admin123")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username)
+                };
+
+                var key = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                var creds = new SigningCredentials(
+                    key,
+                    SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    audience: _configuration["Jwt:Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return Unauthorized("Invalid Username or Password");
+        }
+    }
+}
---
+++ Microservices/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok("Welcome! You have accessed a protected API.");
+        }
+    }
+}
